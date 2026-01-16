a45254fdf8b8c12eac89c035b378ac6246070079
kukiNica <m.bueno.nsp@gmail.com>
2026-01-16 13:56:19 +0100

    Push 2 16.01.2026
    Icon completed 1

--- The Realms of Aetheria Client/Assets/Scripts/IconController.cs
+++ The Realms of Aetheria Client/Assets/Scripts/IconController.cs
@@ -23,4 +23,24 @@ public class IconController : MonoBehaviour
     {
         profilePic.sprite = pic1.icon;
     }
+    public void Icon2()
+    {
+        profilePic.sprite = pic2.icon;
+    }
+    public void Icon3()
+    {
+        profilePic.sprite = pic3.icon;
+    }
+    public void Icon4()
+    {
+        profilePic.sprite = pic4.icon;
+    }
+    public void Icon5()
+    {
+        profilePic.sprite = pic5.icon;
+    }
+    public void Icon6()
+    {
+        profilePic.sprite = pic6.icon;
+    }
 }
